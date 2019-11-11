6bb2bfdfb5ba00a55222339a7ad9aa0e9bd82147
Marija <mari4w04@stud.kea.dk>
2019-11-11 10:51:56 +0100

    Updates

---
+++ precache-manifest.b6d8cbf9b6e2816be5cd3adcfad7bbcf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bad633c1a5a47c31e23847d7673a03",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "6faf8e5ee9dce7ae2a3c",
+    "url": "/weatherapp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ca6a96e9b142618032c8",
+    "url": "/weatherapp/static/css/main.bb37018f.chunk.css"
+  },
+  {
+    "revision": "6faf8e5ee9dce7ae2a3c",
+    "url": "/weatherapp/static/js/2.ff30a10a.chunk.js"
+  },
+  {
+    "revision": "ca6a96e9b142618032c8",
+    "url": "/weatherapp/static/js/main.24cdbc6d.chunk.js"
+  },
+  {
+    "revision": "6addbc205185a49536cd",
+    "url": "/weatherapp/static/js/runtime-main.608b6d07.js"
+  },
+  {
+    "revision": "55b2d40a391eeb2bf03683ce24dc5a2a",
+    "url": "/weatherapp/static/media/bg-1.55b2d40a.jpg"
+  },
+  {
+    "revision": "71ba9a6e56248d97229b86ced7968961",
+    "url": "/weatherapp/static/media/bg-2.71ba9a6e.jpg"
+  },
+  {
+    "revision": "a61bcff41c075fc1339735d76af26113",
+    "url": "/weatherapp/static/media/bg-3.a61bcff4.jpg"
+  },
+  {
+    "revision": "02570aea0e6e601562163983ab1c6f37",
+    "url": "/weatherapp/static/media/cloudy.02570aea.svg"
+  },
+  {
+    "revision": "c1df4de6397510d3ff4dbde7c54e0fe7",
+    "url": "/weatherapp/static/media/mist.c1df4de6.svg"
+  },
+  {
+    "revision": "c00f163079bcf0cbeb56b21277ea7d92",
+    "url": "/weatherapp/static/media/rain.c00f1630.svg"
+  },
+  {
+    "revision": "a90d4febdab1f4305fcf4bb441c2c56a",
+    "url": "/weatherapp/static/media/sun.a90d4feb.svg"
+  },
+  {
+    "revision": "94e7ecf68c39cd63f567d65ad7aed9f9",
+    "url": "/weatherapp/static/media/thunder.94e7ecf6.svg"
+  }
+]);
